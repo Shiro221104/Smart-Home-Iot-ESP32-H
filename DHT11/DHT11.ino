750b9052adbb496e85de9393680d8d9507d3a216
Shiro221104 <Hungnguyen221104@gmail.com>
2026-04-01 16:40:40 +0700

    update

--- DHT11/DHT11.ino
+++ DHT11/DHT11.ino
@@ -1,25 +1,94 @@
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
 #include <DHT.h>
-#include <DHT_U.h>
 
-DHT dht(26, DHT11);
+// ===== DHT =====
+#define DHTPIN 4
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+// ===== WiFi =====
+const char* ssid = "TP-LINK_CC01B8";
+const char* password = "99990000";
+
+// ===== HiveMQ =====
+const char* mqtt_server = "b46a1e0912534437b2b78880fc3cf93a.s1.eu.hivemq.cloud";
+const int mqtt_port = 8883;
+
+// ===== Credentials (từ ảnh của bạn) =====
+const char* mqtt_user = "Hungnguyen221104";
+const char* mqtt_pass = "Hung221104@";
+
+WiFiClientSecure espClient;
+PubSubClient client(espClient);
+
+// ===== Connect MQTT =====
+void connectMQTT() {
+  while (!client.connected()) {
+    Serial.print("Connecting MQTT...");
+
+    if (client.connect("ESP32Client", mqtt_user, mqtt_pass)) {
+      Serial.println("Connected MQTT!");
+    } else {
+      Serial.print("Failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" -> retry...");
+      delay(2000);
+    }
+  }
+}
 
 void setup() {
   Serial.begin(115200);
   dht.begin();
-  delay(2000);
+
+  // ===== Connect WiFi =====
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nWiFi connected");
+  Serial.println(WiFi.localIP());
+
+  // ===== MQTT Setup =====
+  espClient.setInsecure();   // 🔥 BẮT BUỘC
+  client.setServer(mqtt_server, mqtt_port);
 }
 
 void loop() {
-  float temp = dht.readTemperature();
-  float humidity = dht.readHumidity();
+  if (!client.connected()) {
+    connectMQTT();
+  }
+  client.loop();
 
-  Serial.print("Temp: ");
-  Serial.print(temp);
-  Serial.print(" C ");
+  static unsigned long lastMsg = 0;
 
-  Serial.print("Humidity: ");
-  Serial.print(humidity);
-  Serial.println(" % ");
+  if (millis() - lastMsg > 5000) {
+    lastMsg = millis();
 
-  delay(2000);
+    float t = dht.readTemperature();
+    float h = dht.readHumidity();
+
+    if (isnan(t) || isnan(h)) {
+      Serial.println("DHT read failed!");
+      return;
+    }
+
+    String temp = String(t);
+    String hum = String(h);
+
+    // ===== Publish =====
+    client.publish("esp32/dht11/temperature", temp.c_str());
+    client.publish("esp32/dht11/humidity", hum.c_str());
+
+    // ===== Debug =====
+    Serial.println("===== MQTT SENT =====");
+    Serial.println("Temp: " + temp + " °C");
+    Serial.println("Hum : " + hum + " %");
+  }
 }
